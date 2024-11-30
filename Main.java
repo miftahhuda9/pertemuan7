729b38c885591a4720fc419157a53c974c8f50ba
miftahhuda9 <miftahhuda900@gmail.com>
2024-11-30 10:06:27 +0700

    add new code

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
-public class Main
+public class Main{
 
     public static void main(String[] args){
         System.out.println("Hello World")
+
+        String[] books = {"Java Programming, Database Design", "Web Development"};
+        for (String book : books){
+            System.out.println("- " + book);
+        }
     }
+
+}
